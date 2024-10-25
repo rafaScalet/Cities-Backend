329906f63a9079e7745ae74590deca55b64f151d
rafaScalet <rafaelscalet2018@gmail.com>
2024-10-25 16:14:39 -0300

    feat: add city entity

---
+++ src/main/java/com/cities/backend/entities/City.java
@@ -0,0 +1,77 @@
+package com.cities.backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class City {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String name;
+  private String state;
+  private String country;
+  private Integer age;
+  private Double area;
+  private Integer population;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public Double getArea() {
+    return area;
+  }
+
+  public void setArea(Double area) {
+    this.area = area;
+  }
+
+  public Integer getPopulation() {
+    return population;
+  }
+
+  public void setPopulation(Integer population) {
+    this.population = population;
+  }
+
+}
