cae7f68b2bc02418585667c8ae4343b7c51abd9c
rafaScalet <rafaelscalet2018@gmail.com>
2024-10-26 11:00:54 -0300

    feat: add h2 database config and default values

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO city VALUES(10, 100, default, 1000, 'país 1', 'cidade 1', 'estado 1');
+INSERT INTO city VALUES(20, 200, default, 2000, 'país 2', 'cidade 2', 'estado 2');
+INSERT INTO city VALUES(30, 300, default, 3000, 'país 3', 'cidade 3', 'estado 3');
